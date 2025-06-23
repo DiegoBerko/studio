8f162c0a121ff339ef635d5a74548c9adcddee4e
Diego Gabriel Berkovics <dberkovics@despegar.com>
2025-06-23 18:41:10 +0000

    Al elegir el jugador en el modal de asignacion nde goles, se me cierra

--- src/components/controls/goal-scorer-dialog.tsx
+++ src/components/controls/goal-scorer-dialog.tsx
@@ -133,7 +133,10 @@ export function GoalScorerDialog({
               <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
             </Button>
           </PopoverTrigger>
-          <PopoverContent className="w-[--radix-popover-trigger-width] p-0">
+          <PopoverContent 
+            className="w-[--radix-popover-trigger-width] p-0"
+            onCloseAutoFocus={(e) => e.preventDefault()}
+          >
             <Command shouldFilter={false}>
               <CommandInput
                 placeholder="Buscar Nº o Nombre..."
